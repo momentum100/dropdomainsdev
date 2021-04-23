5f0c6ae5f7cadb0cd21cbd093c1d3cc779cc34e4
General M <51231797+momentum100@users.noreply.github.com>
2021-04-24 05:53:16 +0800

    certbot cmd

--- generate-certbot-cmd.php
+++ generate-certbot-cmd.php
@@ -1,17 +1,22 @@
 <?php
 $tmpl = file_get_contents ("vhost-template.txt");
 $serverIP = file_get_contents("http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address");
+$i=0;
+
 
 if ($handle = opendir('/var/www/')) {
         while (false !== ($entry = readdir($handle))) {
                 if ($entry != "." && $entry != "..") {
 
                         if (gethostbyname($entry) == $serverIP) {
-                        $vhost = str_replace ("DOMAIN", $entry, $tmpl);
-                        echo $vhost . "\n";
+   echo "certbot certonly -n --agree-tos --no-redirect --apache --register-unsafely-without-email -d $entry -w /var/www/$entry\n" ;
+	$i++; 
                         }
                 }
         }
 }
+
+echo $i;
 closedir($handle);
+
 
